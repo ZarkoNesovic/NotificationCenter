0ad04692f1d57aa81198589ba6ef627de8328241
ZarkoNesovic <42474678+ZarkoNesovic@users.noreply.github.com>
2022-01-08 16:54:48 +0100

    Large Icon and Big Text

--- app/src/main/java/com/example/notificationcenter/MainActivity.java
+++ app/src/main/java/com/example/notificationcenter/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.core.app.NotificationManagerCompat;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -72,6 +74,39 @@ public class MainActivity extends AppCompatActivity {
                 .setSmallIcon(R.drawable.ic_baseline_airplanemode_active_24)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setColor(Color.BLUE)
+                .setContentIntent(contntIntent)
+                .addAction(R.mipmap.ic_launcher,"Toast",actionIntent)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true)
+                .build();
+        notificationManager.notify(1,notification);
+    }
+    public void sendOnChannel4(View view) {
+        String title=TitleView.getText().toString();
+        String message= MessageView.getText().toString();
+
+        Intent activityIntent=new Intent(this,MainActivity.class);
+        PendingIntent contntIntent=PendingIntent.getActivity(this,0,activityIntent,0);
+
+        Intent broadcastIntent=new Intent(this,NotificationReciver.class);
+        broadcastIntent.putExtra("toastMessage",message);
+        PendingIntent actionIntent=PendingIntent.getBroadcast(this,0,broadcastIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Bitmap largeIcon= BitmapFactory.decodeResource(getResources(),R.drawable.android_large_icon);
+
+        Notification notification=new NotificationCompat.Builder(this,CHANNEL_1_ID)
+                .setSmallIcon(R.drawable.ic_baseline_airplanemode_active_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setLargeIcon(largeIcon)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(getString(R.string.velika_notifikacija_tekst))
+                        .setBigContentTitle(getString(R.string.DesankaMaksimovic))
+                        .setSummaryText("Tekst velika notifikacija sazetak"))
+
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
                 .setColor(Color.BLUE)
